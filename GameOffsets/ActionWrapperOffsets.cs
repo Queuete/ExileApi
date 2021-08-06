381c44da1b06728d1fe5589fa7217b06fa878e98
wlastas <wlastas@gmail.com>
2021-08-06 13:22:27 +0300

    Update ActionWrapperOffsets.cs

--- GameOffsets/ActionWrapperOffsets.cs
+++ GameOffsets/ActionWrapperOffsets.cs
@@ -6,7 +6,7 @@ namespace GameOffsets
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct ActionWrapperOffsets
     {
-        [FieldOffset(0xB0)] public long Target;
+        [FieldOffset(0xD0)] public long Target;
         [FieldOffset(0x150)] public long Skill;
         [FieldOffset(0x170)] public Vector2 Destination;
     }
