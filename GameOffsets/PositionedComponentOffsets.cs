80f0df79cdd29f681a7887832e9aebba0cc92f3d
wlastas <wlastas@gmail.com>
2021-12-22 04:57:42 +0300

    Update PositionedComponentOffsets.cs
    please help correct Reaction - its wrong now:)(

--- GameOffsets/PositionedComponentOffsets.cs
+++ GameOffsets/PositionedComponentOffsets.cs
@@ -7,16 +7,16 @@ namespace GameOffsets
     public struct PositionedComponentOffsets
     {
         [FieldOffset(0x8)] public long OwnerAddress;
-        [FieldOffset(0x159)] public byte Reaction;
+        [FieldOffset(0x159)] public byte Reaction; //<== wrong 3.16.2b
         [FieldOffset(0x1B0)] public Vector2 PrevPosition;
         [FieldOffset(0x1C8)] public Vector2 RelativeCoord;
-        [FieldOffset(0x1E8)] public int GridX;
-        [FieldOffset(0x1EC)] public int GridY;
+        [FieldOffset(0x268)] public int GridX; // 3.16.2b
+        [FieldOffset(0x26C)] public int GridY; // 3.16.2b
         [FieldOffset(0x1F0)] public float Rotation;
         [FieldOffset(0x208)] public float Scale;
         [FieldOffset(0x20C)] public int Size;
         [FieldOffset(0x218)] public Vector2 WorldPosition;
-        [FieldOffset(0x218)] public float WorldX;
-        [FieldOffset(0x21C)] public float WorldY;
+        [FieldOffset(0x22C)] public float WorldX; // 3.16.2b
+        [FieldOffset(0x230)] public float WorldY; // 3.16.2b
     }
 }
