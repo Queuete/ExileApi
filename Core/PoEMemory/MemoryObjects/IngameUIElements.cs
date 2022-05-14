53cf13f2f1d8ddce1435ce311da724501701d7a1
Snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-05-13 20:01:31 -0700

    3.18 Offsets 2/N

--- Core/PoEMemory/MemoryObjects/IngameUIElements.cs
+++ Core/PoEMemory/MemoryObjects/IngameUIElements.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using ExileCore.PoEMemory.Elements;
@@ -98,9 +98,13 @@ namespace ExileCore.PoEMemory.MemoryObjects
         public DivineFont LabyrinthDivineFontPanel =>
             GetObject<DivineFont>(IngameUIElementsStruct.LabyrinthDivineFontPanel);
 
-        public IEnumerable<QuestState> GetUncompletedQuests => GetQuestStates.Where(q => q.QuestStateId != 0);
-        public IEnumerable<QuestState> GetCompletedQuests => GetQuestStates.Where(q => q.QuestStateId == 0);
-        public List<QuestState> GetQuestStates => _cachedQuestStates?.Value;
+        // TODO: Was causing crash. Fix for 3.18.
+        public IEnumerable<QuestState> GetUncompletedQuests => new List<QuestState>(); //GetQuestStates.Where(q => q.QuestStateId != 0));
+
+        public IEnumerable<QuestState> GetCompletedQuests => new List<QuestState>(); //GetQuestStates.Where(q => q.QuestStateId == 0);
+
+        public List<QuestState> GetQuestStates => new List<QuestState>(); //_cachedQuestStates?.Value;
+
 
         private List<QuestState> GenerateQuestStates()
         {
