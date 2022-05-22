11cc6c77754c16ed12981f94774cef0fa928c1d5
Snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2022-05-21 22:52:05 -0700

    StashElement AllInventories updated.
    Added Gem and Flask tabs. Updated remaining tabs which saw some layout changes.

--- Core/PoEMemory/Elements/InventoryElements/GemInventoryItem.cs
+++ Core/PoEMemory/Elements/InventoryElements/GemInventoryItem.cs
@@ -2,10 +2,8 @@
 {
     using SharpDX;
 
-    public class FragmentInventoryItem : NormalInventoryItem
+    public class GemInventoryItem : NormalInventoryItem
     {
-        // Inventory Position in Currency Stash is always invalid.
-        // Also, as items are fixed, so Inventory Position doesn't matter.
         public override int InventPosX => 0;
 
         public override int InventPosY => 0;
